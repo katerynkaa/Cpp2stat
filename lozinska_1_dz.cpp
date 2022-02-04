877a481a1d71731858ad469e278f853b872e322a
katerynkaa <98877313+katerynkaa@users.noreply.github.com>
2022-02-04 17:11:37 +0200

    Add files via upload

---
+++ lozinska_1_dz.cpp
@@ -0,0 +1,10 @@
+﻿//task 1.10
+#include <stdio.h>
+
+int main() {
+	int x1 = 1, x2 = 2, x3 = 3, x4 = 4, x5= 5;
+	float y1 = 6, y2 = 7, y3 = 8, y4 = 9, y5 = 10;
+	printf("x |  %d  |  %d  |  %d  |  %d  |  %d  |\n", x1, x2, x3, x4, x5);
+	printf("- | --  | --  | --  | --  | --  |\n");
+	printf("y | %.2f| %.2f| %.2f| %.2f| %.2f|\n", y1, y2, y3, y4, y5);
+}
