533344b84ec62c731a2ed96a70c672626a3c28e7
katerynkaa <98877313+katerynkaa@users.noreply.github.com>
2022-02-05 00:41:29 +0200

    Homework 3 - Kateryna Lozinska

---
+++ lozinska_3_1.cpp
@@ -0,0 +1,23 @@
+﻿// task 2.17c
+
+#include <stdio.h>
+#include <cmath>
+
+double softSign(double x) {
+	double y;
+	y = x / (1 + fabs(x));
+	return y;
+}
+
+double softSign_der(double x) {
+	double y;
+	y = 1 / (1 + fabs(x) * (1 + fabs(x)));
+	return y;
+}
+
+int main() {
+
+	printf("this function in x0 is %g, derivative in this x0 is %g\n", softSign(-1), softSign_der(-1));
+	printf("this function in x0 is %g, derivative in this x0 is %g\n", softSign(1), softSign_der(1));
+	printf("this function in x0 is %g, derivative in this x0 is %g\n", softSign(0), softSign_der(0));
+}
