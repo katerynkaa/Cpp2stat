2846ec88144ef436a375e7d15de5c47ac6455f04
katerynkaa <98877313+katerynkaa@users.noreply.github.com>
2022-02-22 22:49:06 +0200

    Homework 2 - Lozinska Kateryna

---
+++ lozinska_1_24.cpp
@@ -0,0 +1,17 @@
+﻿//task 1.24
+
+#include <stdio.h>
+#include <string>
+#include <iostream>
+
+
+int main(){
+
+    int n=2, m=5;
+    std::string s1, s2;
+
+    s1.insert(s1.begin(), n - s1.length(), ' ');
+    s2.insert(s2.begin(), 2*n - s2.length(), ' ');
+    std::cout << s1 <<  m << std::endl;
+    std::cout << s2 << m*m << std::endl;
+}
