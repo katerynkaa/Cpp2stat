b4804bbccda24796d3b3c9dc4759b5652e71517a
katerynkaa <98877313+katerynkaa@users.noreply.github.com>
2022-02-22 21:47:38 +0200

    Classwork 1 - Lozinska Kateryna
    I appologise for being late, didn`t have the access to my computer since Thursday. All files were created a week ago, screenshots will be sent by e-mail if needed.
    I didn`t understand the purpose of task 1.5 as well. How can I show x^4 in only 2 steps, if x^4=x^2*x^2=x*x*x*x, which makes it 4 staps already.. anyways, I have completed all the points in this excercise, except x^64, due to the misunderstanding, described below.

---
+++ lozinska_1_1.cpp
@@ -0,0 +1,16 @@
+﻿//task 1.4
+
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+	double m1, m2, r, f;
+
+	printf("m1, m2, r = ");
+	scanf_s("%lf %lf %lf", &m1, &m2, &r);
+	double y = 6.673 * pow(10, -11);
+
+	f = (y * m1 * m2) / r * r;
+	printf("F= %e", f);
+
+}
---
+++ lozinska_1_2.cpp
@@ -0,0 +1,27 @@
+﻿// task 1.5
+
+#include <stdio.h>
+
+int main() {
+	double x = 2, x2, x3, x4, x6, x9, x15, x28, x64;
+
+	x2 = x * x;
+	x3 = x * x2; 
+	x4 = x2 * x2; 
+	x6 = x4 * x2;
+	x9 = x6 * x3;
+	x15 = x9 * x6;
+	x28 = x15 * x9 * x4;
+	x64 = 0;
+
+
+
+
+	printf("x^4=%g \n", x4);
+	printf("x^6=%g \n", x6);
+	printf("x^9=%g\n", x9);
+	printf("x^15=%g\n", x15);
+	printf("x^28=%g\n", x28);
+	printf("x^64=%g\n", x64);
+
+}
