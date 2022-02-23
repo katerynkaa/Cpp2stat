d68e7e7d2f3141e0cd189dbfd2e5fac640f18747
katerynkaa <98877313+katerynkaa@users.noreply.github.com>
2022-02-23 14:55:51 +0200

    Classwork 7 - Lozinska Kateryna

---
+++ lozinska_5_6.cpp
@@ -0,0 +1,18 @@
+﻿#include <stdio.h>
+
+int main() {
+	double b, bk;
+	int n;
+
+	printf("b n=");
+	scanf_s("%lf", &b);
+	printf("n=\n");
+	scanf_s("%i", &n);
+
+	bk = b;
+	for (int i = 0; i < n; i++) {
+		bk = b + 1 / bk;
+	}
+	printf("b_n for n = %i is %lf", n, bk);
+
+}
