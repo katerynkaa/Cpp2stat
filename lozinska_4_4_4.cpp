ab0402118d62d0bb26ea584739f82340dc409fcc
katerynkaa <98877313+katerynkaa@users.noreply.github.com>
2022-04-06 18:15:47 +0300

    Classwork 5 - Lozinska Kateryna

---
+++ lozinska_4_4_4.cpp
@@ -0,0 +1,20 @@
+﻿#include <stdio.h>
+#include <math.h>
+
+int main() {
+	int n;
+	double x, res;
+
+	printf("n=");
+	scanf_s("%d", &n);
+
+	printf("x=");
+	scanf_s("%lf", &x);
+
+	res = 0;
+	for (int i = 1; i <= n; i++) {
+		res += i * pow(x, i);
+	}
+
+	printf("res=%lf", res);
+}
---
+++ lozinska_4_6_6.cpp
@@ -0,0 +1,14 @@
+﻿#include <stdio.h>
+#include <math.h>
+
+int main() {
+	int n;
+	double x=sqrt(2), xn;
+
+	scanf_s("%d", &n);
+	for (int i = 0; i < n; i++) {
+		xn = sqrt(2 + x);
+		x = xn;
+	}
+	printf("n = %lf, y = %lf", n, x);
+}
