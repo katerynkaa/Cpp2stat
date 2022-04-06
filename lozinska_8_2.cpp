309f240bca52b6f0ec68ddc1b4418a213400be20
katerynkaa <98877313+katerynkaa@users.noreply.github.com>
2022-04-06 16:21:42 +0300

    Classwork 8,9 - Lozinska Kateryna

---
+++ lozinska_8_2.cpp
@@ -0,0 +1,33 @@
+﻿#include <stdio.h>
+
+#define N 3
+#define M 3
+
+bool change(double mas[N][M], size_t i, size_t j, double a) {
+	if (i >= N || j >= M) return false;
+
+	mas[i][j] = a;
+	return true;
+}
+
+void output(double mas[N][M]) {
+	for (size_t i = 0; i < N; i++) {
+		for (size_t j = 0; j < M; j++) {
+			printf("a[%zu][%zu]=%lf\n", i, j, mas[i][j]);
+		}
+
+	}
+
+}
+
+int main() {
+	double mas[N][M] = { {1,2,3}, {4,5,6},{7,8,9} };
+
+	size_t i, j;
+	double a;
+	printf("i, j, a =");
+	scanf_s("%zu %zu %lf", &i, &j, &a);
+
+	output(mas);
+
+}
