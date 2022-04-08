74457f901c65375521d9c7e55d770b06755b7ea8
katerynkaa <98877313+katerynkaa@users.noreply.github.com>
2022-04-08 20:21:40 +0300

    Homework 6 - Lozinska Kateryna

---
+++ 4.27.cpp
@@ -0,0 +1,25 @@
+﻿#include <stdio.h>
+#include <math.h>
+
+int main() {
+	double res = 2, u = 2, d = 0, eps = 0.001, res1 = 0;
+	int i = 1;
+
+	while (fabs(res - res1) > eps) {
+		i++;
+		if (i % 2 != 0) {
+			u = u + 2;
+			d = u - 1;
+
+			printf("%lf %lf %lf\n", u, d, res);
+		}
+		else {
+			d = u + 1;
+			printf("%lf %lf %lf\n", u, d, res);
+		}
+		res1 = res;
+		res *= u / d;
+
+	}
+		printf("Approximation to eps = %lf is %lf", eps, res);
+}
