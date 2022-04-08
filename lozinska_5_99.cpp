988fb960b0575fe46d8dacc1328bc85f74d657aa
katerynkaa <98877313+katerynkaa@users.noreply.github.com>
2022-04-08 22:54:37 +0300

    Homework 6 - Lozinska Kateryna

---
+++ lozinska_5_99.cpp
@@ -0,0 +1,46 @@
+﻿#include <stdio.h>
+double seq1(int n) {
+	double v0 = 1, v1 = 0.3, vk;
+	for (int k = 2; k <= n; k++) {
+		vk = (k + 2) * v0;
+		//printf("The %d member of the seq is %lf\n", k, vk);
+
+		v0 = v1;
+		v1 = vk;
+			
+	}
+	return vk;
+}
+
+double seq2(int n) {
+	double v0 = 1, v1 = 1, v2 = 1, vk;
+	for (int k = 3; k <= n; k++) {
+		vk = (k + 4) * (v2 - 1) + (k + 5) * v0;
+		//printf("The %d member of the seq is %lf\n", k, vk);
+
+		v0 = v1;
+		v1 = v2;
+		v2 = vk;
+
+	}
+	return vk;
+}
+
+double seq3(int n) {
+	double v0 = 0, v1 = 0, v2 = 1.5, vk;
+	for (int k = 3; k <= n; k++) {
+		vk = ((k - 2) / ((k - 3) * (k - 3) + 1)) * v2 - v1 * v0 + 1;
+		printf("The %d member of the seq is %lf\n", k, vk);
+
+		v0 = v1;
+		v1 = v2;
+		v2 = vk;
+
+	}
+	return vk;
+}
+
+int main() {
+	int n=10;
+	printf("The %d member of the seq is %lf", n, seq3(n));
+}
