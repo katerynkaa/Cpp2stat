523bbc065d1f756716081a839677e146a8bbd393
katerynkaa <98877313+katerynkaa@users.noreply.github.com>
2022-04-17 00:37:59 +0300

    Classwork 10 - Lozinska Kateryna

---
+++ lozinska_6_2.cpp
@@ -0,0 +1,12 @@
+﻿#include <stdio.h>
+
+int main() {
+	int n;
+	unsigned k;
+
+	scanf_s("%d", &n);
+	scanf_s("%u", &k);
+
+	int res = n | (1 << k);
+	printf("r=%d", res);
+}
---
+++ lozinska_6_6.cpp
@@ -0,0 +1,14 @@
+﻿#include <stdio.h>
+
+int main() {
+
+    for (int x = 0; x < 17; x++) {
+
+        int t1 = (x & (x - 1)) > 0;
+        int t2 = (x & -x);
+
+        printf("%d: %d % d \n", x, t1, t2);
+
+    }
+
+}
