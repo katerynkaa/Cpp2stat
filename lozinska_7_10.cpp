0cb30c24fd7a602ba3bda3f3704c39216b385c3c
katerynkaa <98877313+katerynkaa@users.noreply.github.com>
2022-04-23 00:27:50 +0300

    Homework 8 - Lozinska Kateryna
    a)

---
+++ lozinska_7_10.cpp
@@ -0,0 +1,29 @@
+﻿#include <stdio.h>
+
+#define N 1000
+
+int arr(int mas[N], int n) {
+    int min = 0;
+    for (int i = 0; i < n; i++) {
+        int minn = (i + 1) * mas[i];
+        if (minn < min) {
+            min = minn;
+        }
+    }
+    return min;
+}
+
+
+int main() {
+    int mas[N], n;
+
+    for (int i = 0; i < N; i++) {
+        printf("mas[%d] = ", i);
+        scanf_s("%d", &mas[i]);
+        if (mas[i] == 0) {
+            n = i;
+            break;
+        }
+    }
+    printf("Minimun is %d", arr(mas, n));
+}
