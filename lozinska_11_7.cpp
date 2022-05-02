c9e06c859e966ec642db88eb451df760f7b47b7e
katerynkaa <98877313+katerynkaa@users.noreply.github.com>
2022-05-03 02:36:18 +0300

    Homework 13 - Lozinska Kateryna
    d)

---
+++ lozinska_11_7.cpp
@@ -0,0 +1,30 @@
+﻿#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include <conio.h>
+
+#pragma warning(disable : 4996)
+
+int main()
+{
+    FILE* file;
+    int number, m = 0;
+
+    file = fopen("file.txt", "r");
+
+    if (file == NULL)
+    {
+        printf("Can't open file for reading.\n");
+    }
+    else
+    {
+        while (fscanf(file, "%d", &number) == 1) {
+
+            if (abs(number) > abs(m)) m = number;
+            
+        }
+    }
+    printf("Max is %d", m);
+    fclose(file);
+    return 0;
+}
