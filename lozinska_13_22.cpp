341953337ee26c68db53116fa3acdf3b950b8167
katerynkaa <98877313+katerynkaa@users.noreply.github.com>
2022-05-23 23:45:49 +0300

    Homework 17 - Lozinska Kateryna

---
+++ lozinska_13_22.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <string>
+#include <sstream>
+using namespace std;
+
+string charErasor(string word) {
+    char prelast_char = word[word.length() - 2];
+    word.erase(remove(word.begin(), word.end(), prelast_char), word.end());
+    return word;
+}
+
+
+string wordGenerator(string str) {
+    str += " ";
+
+    string res = "", word = "";
+    for (int i = 0; i < str.length(); i++) {
+        if (str[i] != ' ') {
+            word += str[i];
+        }
+        if (str[i + 1] == ' ' && str[i] != ' ') {
+            res += charErasor(word);
+            word = "";
+        }
+        if (str[i] == ' ') {
+            res += str[i];
+        }
+    }
+    return res;
+}
+
+int main() {
+    string str = "assdsd    gikkiif    hodwdy";
+    cout << wordGenerator(str);
+}
