1800ed1d8ad3ccbab42db2404140e283f85e9eb8
katerynkaa <98877313+katerynkaa@users.noreply.github.com>
2022-05-23 21:20:29 +0300

    Classwork 17 - Lozinska Kateryna

---
+++ lozinska_13_2.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <string>
+
+
+std::string newTwoDots(std::string line) {
+    int firstdot = line.find(".");
+    int secdot = line.find(".", firstdot+1);
+
+    if (firstdot == std::string::npos && secdot == std::string::npos) {
+        int spaceend = line.find_first_not_of(" ");
+        line.erase(0, spaceend);
+        std::cout << "PRINTER1" << '\n';
+    }
+    else {
+        if (firstdot != std::string::npos && secdot == std::string::npos) {
+            line.erase(0, firstdot);
+            std::cout << "PRINTER2" << '\n';
+        }
+        else {
+            line.erase(firstdot, secdot - 1);
+            std::cout << "PRINTER3" << '\n';
+        }
+    }
+    return line;
+}
+
+int main() {
+    std::string str = "  Today.is a.good day";
+    std::cout << newTwoDots(str) << '\n';
+    str = "  Today.is a good day";
+    std::cout << std::endl << newTwoDots(str) << '\n';
+    str = "  Today is a good day";
+    std::cout << std::endl << newTwoDots(str) << '\n';
+}
