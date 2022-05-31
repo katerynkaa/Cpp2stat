f440af60bed1c2a0ea71e45327b2be743c8d3e94
katerynkaa <98877313+katerynkaa@users.noreply.github.com>
2022-05-31 21:38:19 +0300

    Homework 22 - Lozinska Kateryna

---
+++ lozinska_19_18.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    vector<int> v1;
+    int l;
+    cout << "Number of elements ";
+    cin >> l;
+    for (int i = 0; i < l; i++) {
+        int el;
+        cin >> el;
+        v1.push_back(el);
+        if (el % 2 != 0 && i <= (l / 2)) v1.push_back(-1);
+        if (el % 2 == 0 && i >= (l / 2)) v1.push_back(-2);
+    }
+
+    auto iter = v1.begin();
+    while (iter != v1.end())
+    {
+        cout << *iter << " ";
+        ++iter;
+    }
+
+}
